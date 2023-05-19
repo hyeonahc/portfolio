2567fc818ca971fd36a5c4c6dc096a6e939698a0
hyeonah <hyeonah.hello@gmail.com>
2023-05-19 16:50:21 -0400

    #3 feat: Create Header and Add img and style

---
+++ components/Header/Header.styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 0.5em 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Logo = styled.div`
+  img {
+    vertical-align: bottom;
+  }
+`
+
+export const Nav = styled.div`
+  display: flex;
+  gap: 1.6em;
+`
+
+export const Menu = styled.div`
+  display: flex;
+  gap: 1em;
+`
+
+export const Social = styled.div`
+  display: flex;
+  gap: 1em;
+`
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,52 @@
+import logo from '@public/logo.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Container, Logo, Menu, Nav, Social } from './Header.styled'
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <>
+      <Container>
+        <Logo>
+          <Link href='/'>
+            <Image
+              src={logo}
+              width={60}
+              height={60}
+              alt='Picture of the author'
+            />
+          </Link>
+        </Logo>
+        <Nav>
+          <Menu>
+            <Link href='/'>home</Link>
+            <Link href='#projects'>projects</Link>
+            <Link href='#about'>about</Link>
+            <Link href='#experience'>experience</Link>
+            <Link href='#skills'>skills</Link>
+            <Link href='#contact'>contact</Link>
+          </Menu>
+          <Social>
+            <Link
+              href='https://www.linkedin.com/in/hyeonahc'
+              target='_blank'>
+              linkedin
+            </Link>
+            <Link
+              href='https://github.com/hyeonahc'
+              target='_blank'>
+              github
+            </Link>
+            <Link
+              href='https://www.behance.net/hyeonahc'
+              target='_blank'>
+              behance
+            </Link>
+          </Social>
+        </Nav>
+      </Container>
+    </>
+  )
 }
 
 export default Header
