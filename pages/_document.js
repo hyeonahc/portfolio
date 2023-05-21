370eacfc95599700275e8ac9a3a62d0d52e30e99
hyeonah <hyeonah.hello@gmail.com>
2023-05-21 16:06:16 -0400

    #3 design: Add Google Fonts and Apply custom fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -25,5 +25,31 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel='preconnect'
+            href='https://fonts.googleapis.com'
+          />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossorigin
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
