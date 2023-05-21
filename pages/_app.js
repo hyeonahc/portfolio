74b61d9e9bd3fc753ce8258d210680573bf609a5
hyeonah <hyeonah.hello@gmail.com>
2023-05-21 18:55:54 -0400

    #5 design: Create theme.js and Add to project

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,18 @@ import Layout from '@components/Layout'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '@styles/globals.css'
+import theme from '@styles/theme'
+import { ThemeProvider } from 'styled-components'
 
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   )
 }
 
---
+++ styles/theme.js
@@ -0,0 +1,34 @@
+const colors = {
+  accent: '#000CFB',
+  font: '#2E2E2E',
+}
+
+const fontSizes = {
+  medium: '1.6rem',
+  extraLarge: '4rem',
+}
+
+const fontWeight = {
+  thin: 100,
+  light: 300,
+  regular: 400,
+  bold: 700,
+  black: 900,
+}
+
+const breakpoints = {
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+  xxl: '1400px',
+}
+
+const theme = {
+  colors,
+  fontSizes,
+  fontWeight,
+  breakpoints,
+}
+
+export default theme
