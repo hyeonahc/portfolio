a86bf8fe31eb43af0217c792863577dc8009a410
hyeonah <hyeonah.hello@gmail.com>
2023-05-21 16:51:17 -0400

    #4 design: Add breakpoints to layout

---
+++ components/Layout/Layout.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 0 10em;
+  @media (max-width: 1400px) {
+    padding: 0 5em;
+  }
+  @media (max-width: 768px) {
+    padding: 0;
+  }
+`
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,11 +1,12 @@
 import Header from '@components/Header'
+import { Container } from './Layout.styled'
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <Container>
       <Header />
       {children}
-    </>
+    </Container>
   )
 }
 
