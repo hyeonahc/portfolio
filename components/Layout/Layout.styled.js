ec8c78c26f70a6af919b3f6751af6fa3196ab0ff
hyeonah <hyeonah.hello@gmail.com>
2023-05-26 14:48:42 -0400

    #8 design: Adjust Layout width and padding

--- components/Layout/Layout.styled.js
+++ components/Layout/Layout.styled.js
@@ -2,14 +2,11 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   border: 1px solid darkgoldenrod;
-  width: ${props => props.theme.breakpoints.xxxl};
+  width: ${props => props.theme.breakpoints.xxl};
   margin: 0 auto;
   @media (max-width: ${props => props.theme.breakpoints.xxxl}) {
     width: auto;
     margin: 0;
-    padding: 0 10em;
-  }
-  @media (max-width: ${props => props.theme.breakpoints.xxl}) {
     padding: 0 5em;
   }
   @media (max-width: ${props => props.theme.breakpoints.xl}) {
