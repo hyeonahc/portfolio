4ee1acd0f462c6c31243f31f30adbb28f25c22e6
hyeonah <hyeonah.hello@gmail.com>
2023-06-03 16:41:11 -0400

    #14 design: Update Social design

--- components/Common/Social/Social.styled.js
+++ components/Common/Social/Social.styled.js
@@ -2,8 +2,9 @@ import styled from 'styled-components'
 
 export const SocialWrapper = styled.div`
   display: flex;
-  gap: 1em;
+  gap: 0.5em;
   a {
     font-size: ${props => props.theme.fontSizes.lg};
+    padding: 0.25em;
   }
 `
