51dce73e66b882e684e6544bb1479f25bd951cfc
hyeonah <hyeonah.hello@gmail.com>
2023-06-24 22:48:27 -0400

    #22 feat: Update tab desgin and feature to stepper

--- components/ExperienceSection/ExperienceSection.styled.js
+++ components/ExperienceSection/ExperienceSection.styled.js
@@ -1,8 +1,4 @@
-import styled from 'styled-components'
-
-export const TitleWrapper = styled.div`
-  display: flex;
-`
+import styled, { keyframes } from 'styled-components'
 
 export const Title = styled.h2`
   font-family: ${props => props.theme.fontFamily.poppins};
@@ -14,35 +10,145 @@ export const Title = styled.h2`
   position: relative;
 `
 
-export const TabWrapper = styled.div`
+export const ExperienceWrapper = styled.div`
   display: flex;
   height: 28em;
   gap: 2em;
 `
 
 export const Tabs = styled.ul`
-  flex-basis: 30%;
+  flex-basis: 20%;
   height: 100%;
-  overflow: scroll;
+  overflow-y: scroll;
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: ${props => props.theme.colors.lightGrey};
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar {
+    width: 8px;
+    background-color: ${props => props.theme.colors.lightGrey};
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: linear-gradient(#000cfb, #4777fc, #81b0f7);
+  }
+`
+
+export const Line = styled.div`
+  background-color: ${props => props.theme.colors.grey};
+  height: 50px;
+  transition: background-color 0.3s ease-in;
+  width: 2px;
+`
+
+export const Indicator = styled.span`
+  align-items: center;
+  background-color: ${props => props.theme.colors.grey};
+  border: calc(16px / 3.5) solid ${props => props.theme.colors.white};
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  height: 16px;
+  transition: background-color 0.3s ease-in;
+  width: 16px;
+`
+
+export const TabButton = styled.div`
+  cursor: pointer;
+  align-items: center;
+  color: ${props => props.theme.colors.grey};
+  display: flex;
+  font-size: 14px;
+  flex-direction: column;
+  flex-shrink: 0;
+  position: relative;
+  transition: color 0.2s ease-in;
+  width: 80px;
+  &:hover {
+    color: ${props => props.theme.colors.primary};
+    ${Indicator} {
+      background-color: ${props => props.theme.colors.primary};
+    }
+  }
+  &:focus-visible {
+    border-radius: 4px;
+    outline: ${props => props.theme.colors.primary} solid 2px;
+  }
+  &:focus {
+    border-radius: 4px;
+    outline: ${props => props.theme.colors.primary} solid 2px;
+  }
+`
+
+export const IconCompleted = styled.svg`
+  fill: transparent;
+`
+
+const fillColorChange = keyframes`
+  0% {
+    fill: ${props => props.theme.colors.primary};
+  }
+  100% {
+    fill: ${props => props.theme.colors.white};
+  }
+`
+
+export const Label = styled.span`
+  font-weight: ${props => props.theme.fontWeight.semibold};
+  margin-top: 4px;
 `
 
 export const Tab = styled.li`
-  border-left: 3px solid ${props => props.theme.colors.grey};
-  cursor: pointer;
-  padding: 1em;
-  &&.active {
-    border-left: 3px solid ${props => props.theme.colors.primary};
-    color: ${props => props.theme.colors.primary};
-    font-weight: ${props => props.theme.fontWeight.semibold};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  &:not(:first-child) {
+    width: 100%;
+  }
+  &:not(.current).navigable ${Indicator} {
+    background-color: ${props => props.theme.colors.primary};
+    transition: all 0.3s ease-in;
+  }
+  &:not(.current).navigable .StepButton:hover ${Indicator} {
+    background-color: ${props => props.theme.colors.primary};
+    transition: all 0.3s ease-in;
+  }
+  &.current {
+    ${Label} {
+      color: ${props => props.theme.colors.primary};
+    }
+    ${Indicator} {
+      background-color: ${props => props.theme.colors.white};
+      border: calc(16px / 4) solid ${props => props.theme.colors.primary};
+      transition: all 0.3s ease-in;
+    }
+  }
+  &.completed {
+    ${Label} {
+      color: ${props => props.theme.colors.primary};
+    }
+    ${Indicator} {
+      border: 0;
+      transition: all 0.3s ease-in;
+    }
+    ${IconCompleted} {
+      animation: ${fillColorChange} 0.2s ease-in 0.3s forwards;
+      fill: ${props => props.theme.colors.white};
+    }
+  }
+  &.navigable {
+    ${Line} {
+      background-color: ${props => props.theme.colors.primary};
+      transition: background-color 0.2s ease-in;
+    }
   }
 `
 
 export const Content = styled.div`
   padding-top: 20px;
-  flex-basis: 70%;
+  flex-basis: 80%;
 `
-
-export const ContentItem = styled.div``
 
 export const Position = styled.h3`
   font-size: ${props => props.theme.fontSizes.xl};
@@ -63,11 +169,7 @@ export const Organization = styled.p`
   }
 `
 
-export const Duration = styled.p``
-
-export const ExperienceUl = styled.ul``
-
-export const ExperienceLi = styled.li`
+export const ExperienceList = styled.li`
   margin-bottom: 5px;
   svg {
     font-size: ${props => props.theme.fontSizes.xs};
--- components/ExperienceSection/index.js
+++ components/ExperienceSection/index.js
@@ -5,25 +5,55 @@ import Link from 'next/link'
 import { useState } from 'react'
 import {
   Content,
-  ContentItem,
-  Duration,
-  ExperienceLi,
-  ExperienceUl,
+  ExperienceList,
+  ExperienceWrapper,
+  IconCompleted,
+  Indicator,
+  Label,
+  Line,
   Organization,
   OrganizationDurationWrapper,
   Position,
   Tab,
-  TabWrapper,
+  TabButton,
   Tabs,
   Title,
 } from './ExperienceSection.styled'
 import { MyJourneys } from './MyJourneys'
 
 const ExperienceSection = () => {
-  const [currentTabIndex, setCurrentTabIndex] = useState('1')
+  const [currentTabIndex, setCurrentTabIndex] = useState(0)
+  const [currentStep, setCurrentStep] = useState(1)
+  const [completedStep, setCompletedStep] = useState(0)
 
-  const clickTab = e => {
-    setCurrentTabIndex(e.target.id)
+  const disableNavigation = false
+
+  const isStepCurrent = index => index + 1 === currentStep
+  const isStepCompleted = index =>
+    index + 1 !== currentStep && completedStep >= index + 1
+  const isStepNavigable = index => completedStep >= index
+
+  const getStepClassNames = index => {
+    let result
+    if (currentTabIndex === index) {
+      result = 'active'
+    }
+    if (isStepCurrent(index)) {
+      result = `${result} current`
+    }
+    if (isStepCompleted(index)) {
+      result = `${result} completed`
+    }
+    if (isStepNavigable(index)) {
+      result = `${result} navigable`
+    }
+    return result
+  }
+
+  const clickTab = index => {
+    setCurrentTabIndex(index)
+    setCurrentStep(index + 1)
+    setCompletedStep(index)
   }
 
   return (
@@ -39,22 +69,38 @@ const ExperienceSection = () => {
             right='-15px'
           />
         </Title>
-        <TabWrapper>
+        <ExperienceWrapper>
           <Tabs>
             {MyJourneys.map((myJourney, index) => (
               <Tab
                 key={index}
                 id={index}
-                className={currentTabIndex === `${index}` && 'active'}
-                onClick={clickTab}>
-                {myJourney.organization}
+                className={getStepClassNames(index)}>
+                {index > 0 && <Line />}
+                <TabButton
+                  disabled={disableNavigation && !isStepNavigable(index)}
+                  onClick={() => clickTab(index)}>
+                  <Indicator>
+                    {isStepCompleted(index) && (
+                      <IconCompleted
+                        width='10'
+                        height='7'
+                        viewBox='0 0 12 9'
+                        fill='currentColor'>
+                        <path d='M1.05025 3.70714C1.44077 3.31661 2.07394 3.31661 2.46446 3.70714L5.29289 6.53556C5.68341 6.92609 5.68341 7.55925 5.29289 7.94978C4.90236 8.3403 4.2692 8.3403 3.87867 7.94978L1.05025 5.12135C0.659724 4.73083 0.659724 4.09766 1.05025 3.70714Z' />
+                        <path d='M10.9498 0.878709C11.3403 1.26923 11.3403 1.9024 10.9498 2.29292L5.29289 7.94978C4.90236 8.3403 4.2692 8.3403 3.87867 7.94978C3.48815 7.55925 3.48816 6.92609 3.87869 6.53556L9.53554 0.878709C9.92606 0.488184 10.5592 0.488184 10.9498 0.878709Z' />
+                      </IconCompleted>
+                    )}
+                  </Indicator>
+                  <Label>{myJourney.year}</Label>
+                </TabButton>
               </Tab>
             ))}
           </Tabs>
           <Content>
             {MyJourneys.map((myJourney, index) => (
-              <ContentItem key={index}>
-                {currentTabIndex === `${index}` && (
+              <div key={index}>
+                {currentTabIndex === index && (
                   <>
                     <Position>{myJourney.position}</Position>
                     <OrganizationDurationWrapper>
@@ -63,22 +109,22 @@ const ExperienceSection = () => {
                           @ {myJourney.organization}
                         </Link>
                       </Organization>
-                      <Duration>{myJourney.duration}</Duration>
+                      <p>{myJourney.duration}</p>
                     </OrganizationDurationWrapper>
-                    <ExperienceUl>
+                    <ul>
                       {myJourney.experiences.map((experience, index) => (
-                        <ExperienceLi key={index}>
+                        <ExperienceList key={index}>
                           <FontAwesomeIcon icon={faPlay} />
                           {experience}
-                        </ExperienceLi>
+                        </ExperienceList>
                       ))}
-                    </ExperienceUl>
+                    </ul>
                   </>
                 )}
-              </ContentItem>
+              </div>
             ))}
           </Content>
-        </TabWrapper>
+        </ExperienceWrapper>
       </Col>
     </Row>
   )
